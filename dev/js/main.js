caafaecfc18d2094439ef28f7152e5dd44fa25a1
Sklyarov Alexey <metamarfoz.sk@gmail.com>
2020-08-04 11:32:16 +0300

    Added order modal

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,8 +1,9 @@
 let Order = {
-
-  init: function(){
-    $('.js-date-mask').mask("99",{placeholder:" ",});
-    $('.js-cvv-mask').mask("999",{placeholder:" ",});
+  /**
+   * @module       Card
+   * @description  Инициализация поиска типа кредитной карты по номеру
+   */
+  initCardInfo: function () {
     CardInfo.setDefaultOptions({
       banksLogosPath: '/images/banks-logos/',
       brandsLogosPath: '/images/brands-logos/'
@@ -17,18 +18,61 @@ let Order = {
         }
       });
     });
+  },
 
+  /**
+   * @module       Mask
+   * @description  Инициализация масок
+   */
+  initMasks: function (){
+    $('.js-date-mask').mask("99",{placeholder:" ",});
+    $('.js-cvv-mask').mask("999",{placeholder:" ",});
+  },
+
+  /**
+   * @module       Handlers
+   * @description  Инициализация обработчиков
+   */
+  initHandlers: function () {
+    // ------
+    // Обработка клика по кнопке показа формы
+    // ------
     $('.js-show-cardform').on('click',function(e){
       e.preventDefault();
       $('.orderform__cardpay').addClass('orderform__cardpay--active');
     });
 
+    // ------
+    // Обработка ввода данных в поле ввода почты
+    // ------
     $('#order-usermail').on('change keyup',function(){
       if (this.value.length > 1) {
         $('.orderform__creditbutton,.orderform__gpaybutton').prop('disabled',false);
       } else {
         $('.orderform__creditbutton,.orderform__gpaybutton').prop('disabled',true);
       }
+    });
+    // ------
+    // Обработка ввода данных в поле ввода почты
+    // ------
+    $('.popup__close').on('click',function(){
+      let popup = $(this).attr('data-popup');
+      $(popup).removeClass('popup-wrapper--visible');
+    });
+
+    $('[data-open="modal"]').on('click',function () {
+      let modal = $(this).attr('data-target');
+      $('.popup').removeClass('popup-wrapper--visible');
+      $(modal).addClass('popup-wrapper--visible');
     })
+  },
+  /**
+   * @module       Init
+   * @description  Инициализация основных модулей
+   */
+  init: function(){
+    this.initHandlers();
+    this.initMasks();
+    this.initCardInfo();
   }
 }
